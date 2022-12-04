d994ce03272092411405b34b2e32f4ba17d65ab7
GistOfSpirit <daphazard@gmail.com>
2022-12-04 00:19:06 +0000

    build.sh works from any directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-mkdir -p build
-rm -R build/*
-cp ./*.qml build/
-cp -r components build/
-cp metadata.desktop build/
-cp preview.png build/
-cp LICENSE build/
-cp theme.conf build/
-mv build/RealProxy.qml build/Proxy.qml
+SCRIPT_LOC=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_LOC")
+
+rm -Rf "$SCRIPT_DIR"/../build
+mkdir -p "$SCRIPT_DIR"/../build
+cp "$SCRIPT_DIR"/../*.qml "$SCRIPT_DIR"/../build/
+cp -r "$SCRIPT_DIR"/../components "$SCRIPT_DIR"/../build/
+cp "$SCRIPT_DIR"/../metadata.desktop "$SCRIPT_DIR"/../build/
+cp "$SCRIPT_DIR"/../preview.png "$SCRIPT_DIR"/../build/
+cp "$SCRIPT_DIR"/../LICENSE "$SCRIPT_DIR"/../build/
+cp "$SCRIPT_DIR"/../theme.conf "$SCRIPT_DIR"/../build/
+mv "$SCRIPT_DIR"/../build/RealProxy.qml "$SCRIPT_DIR"/../build/Proxy.qml
